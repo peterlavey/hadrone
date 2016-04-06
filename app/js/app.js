0543f0f55a623bacddae7789c32e5a9c7f420e89
Pedro Cisternas (Work) <pedrocisternasd@gmail.com>
2016-04-06 12:51:28 -0300

    ui router limpio

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,15 @@
 define(['angular', 'uiRouter', './controllers/controller', './directives/directive', './services/service'], (ng)=>{
   'use strict';
   return ng.module('app', ['ui.router', 'app.controllers', 'app.directives', 'app.services'])
-    .config(['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider)=> {
-      $stateProvider.state('dashboard',{
-        url: '/dashboard',
-        templateUrl: 'components/dashboard.html'
-      })
-      .state('home',{
-            url: '/home',
-            templateUrl: 'components/home.html'
-        })
-        $urlRouterProvider.otherwise('/dashboard');
+  .config(['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider)=> {
+    $stateProvider.state('dashboard',{
+      url: '/dashboard',
+      templateUrl: 'components/dashboard.html'
+    })
+    .state('home',{
+      url: '/home',
+      templateUrl: 'components/home.html'
+    })
+    $urlRouterProvider.otherwise('/dashboard');
   }]);
 });
